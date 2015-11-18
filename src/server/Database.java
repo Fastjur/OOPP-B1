5cdee18cea5780dd80e7cbd0a5e730b7c0bb6fa7
Jurriaan Den Toonder <speedy4104@hotmail.com>
2015-11-19 00:55:02 +0100

    Made some very basic db read functions

--- src/server/Database.java
+++ src/server/Database.java
@@ -1,7 +1,44 @@
 package server;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.*;
+
 /**
- * Created by Fastjur on 19-11-2015.
+ * Created by Fastjur on 18-11-2015.
  */
 public class Database {
+
+    private JSONObject db;
+
+    public Database(String file) throws IOException {
+        try {
+            this.db = readDB(file);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private JSONObject readDB(String file) throws IOException, JSONException {
+        String jsonData = readFile(file);
+        JSONObject jobj = new JSONObject(jsonData);
+        return jobj;
+    }
+
+    private String readFile(String file) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        StringBuilder sb = new StringBuilder();
+        String line = br.readLine();
+        while(line != null) {
+            sb.append(line);
+            line = br.readLine();
+        }
+        return sb.toString().replaceAll("\\s","");
+    }
+
+    public String toString() {
+        return this.db.toString();
+    }
+
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,5 +1,10 @@
 package server;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Main Server Class
  * This file will most likely contain the main method for starting up the server
@@ -7,5 +12,16 @@ package server;
  * @version 0.1
  */
 public class Server {
+
+    private Server server;
+
+    public static void main(final String[] args) {
+        try {
+            Database db = new Database("src/server/database.txt");
+            System.out.println(db);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
