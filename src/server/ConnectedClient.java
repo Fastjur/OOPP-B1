8a4792c21a3ec5714c87fb0d83121a3f986598e3
Govert de Gans <grappigegovert@hotmail.com>
2015-11-20 20:09:54 +0100

    Added logic for sending/receiving messages

--- src/server/ConnectedClient.java
+++ src/server/ConnectedClient.java
@@ -1,5 +1,7 @@
 package server;
 
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.net.Socket;
 
@@ -15,12 +17,24 @@ public class ConnectedClient {
 
     public ConnectedClient(Socket socket) {
         this.socket = socket;
-        this.connectionThread = new ClientConnectionThread(this.socket);
+        this.connectionThread = new ClientConnectionThread(this);
         this.connectionThread.start();
     }
 
     public void closeConnection() throws IOException {
         this.connectionThread.end();
         this.socket.close();
+    }
+
+    public Socket getSocket() {
+        return this.socket;
+    }
+
+    public void sendMessage(JSONObject message) {
+        try {
+            connectionThread.sendMessage(message);
+        } catch(java.io.IOException ex) {
+            System.out.println("Err: could not send the message\n" + ex.getLocalizedMessage());
+        }
     }
 }
