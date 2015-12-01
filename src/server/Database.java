31c573f131b05ab5c6d3ebd67a78eb4655222a50
Jurriaan Den Toonder <speedy4104@hotmail.com>
2015-12-01 04:23:50 +0100

    Please for the love of god. CHECK YOUR IDE FOR ALERTS Secondly. DO NOT USE TABS FOR INDENTATION, setup your ide to replace a tab with 4 spaces. Because now EVERY SINGLE FILE causes merging errors due to one person using tabs, and the other 4 spaces. USE SPACES

--- src/server/Database.java
+++ src/server/Database.java
@@ -1,6 +1,7 @@
 package server;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 /**
  * Database class that reads the database file
@@ -11,6 +12,7 @@ import java.sql.*;
 public class Database {
 
     private final String JDBCUrl = "jdbc:mysql://db4free.net:3306/ooppb1";
+//    private final String JDBCUrl = "jdbc:mysql://localhost:3306/ooppb1";
     private final String user = "oopp_usr";
     private final String password = "oopp_b1_database";
 
@@ -30,10 +32,15 @@ public class Database {
     }
 
     public User getUser(String email) {
-        User user = new User();
+        User user;
         try {
             //TODO uncomment statement parts when they have been implemented in User.java
-            PreparedStatement stmt = connection.prepareStatement("SELECT users.id, nationalities.name as nationality, studies.name as study, universities.name as university,   email as dbemail, passwd, firstname, lastname, sex, birthdate, bio, studyYear, availableDates, location, phonenumber, photo FROM `users` LEFT JOIN nationalities on users.nationality_id = nationalities.id LEFT JOIN studies ON users.study = studies.id LEFT JOIN universities ON users.universities_id = universities.id WHERE email = ? LIMIT 1");
+            PreparedStatement stmt = connection.prepareStatement("SELECT users.id, nationalities.name as nationality," +
+                    "studies.name as study, universities.name as university, email as dbemail, passwd, firstname," +
+                    "lastname, sex, birthdate, bio, studyYear, availableDates, location, phonenumber, photo " +
+                    "FROM `users` LEFT JOIN nationalities on users.nationality_id = nationalities.id " +
+                    "LEFT JOIN studies ON users.study = studies.id " +
+                    "LEFT JOIN universities ON users.universities_id = universities.id WHERE users.email = ? LIMIT 1");
             stmt.setString(1, email);
             ResultSet rs = stmt.executeQuery();
             user = processUser(rs);
@@ -47,13 +54,16 @@ public class Database {
         User user;
         try {
             //TODO uncomment statement parts when they have been implemented in User.java
-            PreparedStatement stmt = connection.prepareStatement("SELECT id, nationality_id, studies_id," +
-                    "universities_id, email as dbemail, passwd, firstname, lastname, sex, birthdate, study, bio," +
-                    "studyYear, /*availableDates, */location, phonenumber, photo FROM `users` WHERE id = ? " +
-                    "LIMIT 1");
+            PreparedStatement stmt = connection.prepareStatement("SELECT users.id, nationalities.name as nationality," +
+                    "studies.name as study, universities.name as university, email as dbemail, passwd, firstname," +
+                    "lastname, sex, birthdate, bio, studyYear, availableDates, location, phonenumber, photo " +
+                    "FROM `users` LEFT JOIN nationalities on users.nationality_id = nationalities.id " +
+                    "LEFT JOIN studies ON users.study = studies.id " +
+                    "LEFT JOIN universities ON users.universities_id = universities.id WHERE users.id = ? LIMIT 1");
             stmt.setInt(1, id);
             ResultSet rs = stmt.executeQuery();
             user = processUser(rs);
+            stmt.close();
         } catch (SQLException e) {
             throw new IllegalStateException("Could not get user by email:\n" + e.getMessage());
         }
@@ -62,7 +72,7 @@ public class Database {
 
     private User processUser(ResultSet rs) throws SQLException {
         User usr = new User();
-        while(rs.next()) {
+        if(rs.next()) {
             int id = rs.getInt("id"),
                     studyYear = rs.getInt("studyYear");
             String dbemail = rs.getString("dbemail"),
@@ -77,11 +87,44 @@ public class Database {
                     location = rs.getString("location"),
                     phonenumber = rs.getString("phonenumber"),
                     photo = rs.getString("photo");
-
             Date birthdate = new Date(rs.getLong("birthdate"));
 
+            PreparedStatement stmt = connection.prepareStatement("SELECT courses.name FROM courses " +
+                    "LEFT JOIN coursessearchingbuddy ON courses_id = courses.id " +
+                    "WHERE coursessearchingbuddy.users_id = ?");
+            stmt.setInt(1, id);
+            ResultSet rs2 = stmt.executeQuery();
+            ArrayList<String> coursesSearchingBuddy = new ArrayList<String>();
+            while(rs2.next()) {
+                coursesSearchingBuddy.add(rs2.getString("name"));
+            }
+            stmt.close();
+
+            stmt = connection.prepareStatement("SELECT courses.name FROM courses " +
+                    "LEFT JOIN coursesteaching ON courses_id = courses.id " +
+                    "WHERE coursesteaching.users_id = ?");
+            stmt.setInt(1, id);
+            rs2 = stmt.executeQuery();
+            ArrayList<String> coursesTeaching = new ArrayList<String>();
+            while(rs2.next()) {
+                coursesTeaching.add(rs2.getString("name"));
+            }
+            stmt.close();
+
+            stmt = connection.prepareStatement("SELECT courses.name FROM courses " +
+                    "LEFT JOIN courseslearning ON courses_id = courses.id " +
+                    "WHERE courseslearning.users_id = ?");
+            stmt.setInt(1, id);
+            rs2 = stmt.executeQuery();
+            ArrayList<String> coursesLearning = new ArrayList<String>();
+            while(rs2.next()) {
+                coursesLearning.add(rs2.getString("name"));
+            }
+            stmt.close();
+
             usr = new User(id, password, firstname, lastname, birthdate, dbemail, phonenumber,
-                    new Address("A", "B", "C","D"), study, university, studyYear, sex, nationality, bio, location, photo);
+                    new Address("A", "B", "C","D"), study, university, studyYear, new ArrayList(), coursesTeaching,
+                    coursesLearning, coursesSearchingBuddy, sex, nationality, bio, location, photo);
             System.out.println(usr);
         }
         return usr;
