597f3f39b7574d2e7aa1aafd5d9fb2c9d2cb8597
Jurriaan Den Toonder <speedy4104@hotmail.com>
2015-12-02 14:49:55 +0100

    Start of JSON errors fix

--- src/server/ClientConnectionThread.java
+++ src/server/ClientConnectionThread.java
@@ -1,7 +1,5 @@
 package server;
 
-import org.json.JSONObject;
-
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
--- src/server/ConnectedClient.java
+++ src/server/ConnectedClient.java
@@ -1,7 +1,5 @@
 package server;
 
-import org.json.JSONObject;
-
 import java.io.IOException;
 import java.net.Socket;
 import java.util.ArrayList;
