5b74ad179f0decc8df554bf11dee6a7ea51fa9b5
Jurriaan Den Toonder <speedy4104@hotmail.com>
2015-12-02 23:13:38 +0100

    Start of refactoring availability

---
+++ src/server/AvailableTimes.java
@@ -0,0 +1,10 @@
+package server;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Fastjur on 2-12-2015.
+ */
+public class AvailableTimes {
+    private ArrayList<String> days = new ArrayList<String>();
+}
