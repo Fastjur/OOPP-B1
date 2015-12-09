d672e69276cd422a68f6cfa4ab8ce609c911267d
Govert de Gans <grappigegovert@hotmail.com>
2015-12-09 21:43:16 +0100

    Completes change from address/location to longitude/latitude

--- src/server/DudDatabase.java
+++ src/server/DudDatabase.java
@@ -16,10 +16,10 @@ public class DudDatabase extends Database {
     public User getUser(String email) {
         if (email.equals("sinterklaas@sintmail.nl")) {
             return new User(1, "Pepernoten01", "Sinter", "Klaas", new Date(00001),
-                    "sinterklaas@sintmail.nl", "+316123456789", new Address("A", "B", "C", "D"),
+                    "sinterklaas@sintmail.nl", "+316123456789",
                     "study1", "university", 3, new AvailableTimes(), new ArrayList<String>(),
                     new ArrayList<String>(), new ArrayList<String>(), "male", "NLD",
-                    new ArrayList<String>(), "It's-a-me", "Spanje");
+                    new ArrayList<String>(), "It's-a-me", 3, 0);
         }
         return null;
     }
