5f17b8af1baa518634fd31b64bac75a799c022b1
DragonflyCoding <sebastiaanhester@gmail.com>
2015-12-09 13:16:32 +0100

    Latest changes to profile before merge

--- src/client/ProfileGUI.java
+++ src/client/ProfileGUI.java
@@ -1,5 +1,3 @@
-package client;
-
 import javafx.application.Application;
 import javafx.geometry.*;
 import javafx.scene.Scene;
@@ -35,17 +33,17 @@ public class ProfileGUI extends Application {
         Insets topBarBtnInsets = new Insets(40, 20, 0, 20);
         //Insets noPaddingInsets = new Insets(0, 960, 180, 960); -> topBarInsets
         //Insets noPaddingInsets = new Insets(25, 100, 25, 100); -> sideBarBtnInsets
-        Insets sideBarBtnInsets = new Insets(0,0,0,20);
+        Insets sideBarBtnInsets = new Insets(0, 0, 0, 20);
         //Insets noPaddingInsets = new Insets(0, 100, 880, 100); -> sideBarInsets
         //Insets noPaddingInsets = new Insets(15, 205, 15, 205); -> uploadPicBtnInsets
         //Insets noPaddingInsets = new Insets(230, 205, 230, 205); -> profilePicPaneInsets
         //Insets noPaddingInsets = new Insets(25,645,25,645); -> profileInfoTitlePaneInsets
-        Insets profileInfoTitleInsets = new Insets(20,0,10,10);
+        Insets profileInfoTitleInsets = new Insets(20, 0, 10, 10);
         Insets profileInfoInsets = new Insets(15, 20, 20, 10);
         Insets profileAdditionalInfoInsets = new Insets(10);
-        Insets profileAvailabilityTitlePaneInsets = new Insets(10,10,0,10);
-        Insets profileAvailabilityGridPaneInsets = new Insets(0,10,5,10);
-        Insets profileAvailabilityPaneInsets = new Insets(0,0,0,20);
+        Insets profileAvailabilityTitlePaneInsets = new Insets(10, 10, 0, 10);
+        Insets profileAvailabilityGridPaneInsets = new Insets(0, 10, 5, 10);
+        Insets profileAvailabilityPaneInsets = new Insets(0, 0, 0, 20);
         //Insets editToggleBtnInsets = new Insets(20,20,20,20);                     //Might not be needed
         Insets profileInsets = new Insets(10, 30, 30, 30);
 
@@ -55,10 +53,11 @@ public class ProfileGUI extends Application {
         //Buttons:
         Button findMatchBtn = new Button();
         findMatchBtn.setPadding(topBarBtnInsets);
-        findMatchBtn.setMinSize(80,80);
+        findMatchBtn.setMinSize(80, 80);
         findMatchBtn.setPrefSize(80, 80);
-        findMatchBtn.setMaxSize(200,80);
+        findMatchBtn.setMaxSize(200, 80);
         findMatchBtn.setText("Find Match");
+        findMatchBtn.setId("findMatchBtn");
         Button yourMatchesBtn = new Button();
         yourMatchesBtn.setPadding(topBarBtnInsets);
         yourMatchesBtn.setMinSize(80, 80);
@@ -79,28 +78,29 @@ public class ProfileGUI extends Application {
         profileBtn.setText("Profile");
         Button editProfileBtn = new Button();
         editProfileBtn.setPadding(sideBarBtnInsets);
-        editProfileBtn.setMinSize(200,50);
-        editProfileBtn.setPrefSize(200,50);
-        editProfileBtn.setMaxSize(200,50);
+        editProfileBtn.setMinSize(200, 50);
+        editProfileBtn.setPrefSize(200, 50);
+        editProfileBtn.setMaxSize(200, 50);
         editProfileBtn.setText("Edit Profile");
         editProfileBtn.setTextAlignment(TextAlignment.LEFT);
         Button logoutBtn = new Button();
         logoutBtn.setPadding(sideBarBtnInsets);
-        logoutBtn.setMinSize(200,50);
-        logoutBtn.setPrefSize(200,50);
-        logoutBtn.setMaxSize(200,50);
+        logoutBtn.setMinSize(200, 50);
+        logoutBtn.setPrefSize(200, 50);
+        logoutBtn.setMaxSize(200, 50);
         logoutBtn.setText("Logout");
         Button exitBtn = new Button();
         exitBtn.setPadding(noPaddingInsets);
-        exitBtn.setMinSize(40,20);
-        exitBtn.setPrefSize(40,20);
-        exitBtn.setMaxSize(40,20);
+        exitBtn.setMinSize(40, 20);
+        exitBtn.setPrefSize(40, 20);
+        exitBtn.setMaxSize(40, 20);
         exitBtn.setText("X");
+        exitBtn.setOnMouseClicked(e -> System.exit(0));
         Button uploadPicBtn = new Button();
         uploadPicBtn.setPadding(noPaddingInsets);
-        uploadPicBtn.setMinSize(410,30);
-        uploadPicBtn.setPrefSize(410,30);
-        uploadPicBtn.setMaxSize(410,30);
+        uploadPicBtn.setMinSize(410, 30);
+        uploadPicBtn.setPrefSize(410, 30);
+        uploadPicBtn.setMaxSize(410, 30);
         uploadPicBtn.setText("Upload picture");
         Button editToggleBtn = new Button();
         editToggleBtn.setShape(editToggleBtnShape);
@@ -132,19 +132,26 @@ public class ProfileGUI extends Application {
         Label findStudyBuddy = new Label("Find a study buddy:");
         Label availability = new Label("Availability");
         Label monday = new Label("Monday:");
-        Label tuesday= new Label("Tuesday:");
-        Label wednesday= new Label("Wednesday:");
-        Label thursday= new Label("Thursday:");
-        Label friday= new Label("Friday:");
-        Label saturday= new Label("Saturday:");
-        Label sunday= new Label("Sunday:");
+        Label tuesday = new Label("Tuesday:");
+        Label wednesday = new Label("Wednesday:");
+        Label thursday = new Label("Thursday:");
+        Label friday = new Label("Friday:");
+        Label saturday = new Label("Saturday:");
+        Label sunday = new Label("Sunday:");
 
         //Image settings
-        Image profilePic = new Image("../client/ProfilePicTestImage.jpg");
+
+        Image profilePic = new Image("ProfilePicTestImage.jpg");
         ImageView profilePicView = new ImageView(profilePic);
         profilePicView.setFitHeight(460);
         profilePicView.setFitWidth(410);
         profilePicView.setPreserveRatio(true);
+        /*
+        VBox randBox = new VBox();
+        randBox.setMinSize(0,0);
+        randBox.setPrefSize(410,460);
+        randBox.setMaxSize(410,460);
+        */
 
         //Stage settings
         PrimaryStage.setTitle("Sidebar-only version 1.0");
@@ -152,7 +159,7 @@ public class ProfileGUI extends Application {
         PrimaryStage.setMinWidth(800);
         PrimaryStage.setMaxHeight(1080);
         PrimaryStage.setMaxWidth(1920);
-        PrimaryStage.setMaximized(true);
+        PrimaryStage.setFullScreen(true);
 
         //Panes
         HBox exitBarPane = new HBox();
@@ -165,7 +172,7 @@ public class ProfileGUI extends Application {
         topBarButtonsPane.getChildren().addAll(findMatchBtn, yourMatchesBtn, chatBtn, profileBtn);
         topBarButtonsPane.setAlignment(Pos.BASELINE_CENTER);
 
-        HBox topBarPane = new HBox();
+        VBox topBarPane = new VBox();
         topBarPane.setPadding(noPaddingInsets);
         topBarPane.setSpacing(0);
         topBarPane.setAlignment(Pos.TOP_LEFT);
@@ -182,14 +189,16 @@ public class ProfileGUI extends Application {
         profilePicPane.setAlignment(Pos.TOP_LEFT);
         profilePicPane.getChildren().addAll(profilePicView, uploadPicBtn);
 
+        /*
         HBox profileInfoTitlePane = new HBox();
         profileInfoTitlePane.setPadding(profileInfoTitleInsets);
         profileInfoTitlePane.getChildren().addAll(profileInfoLabel);
+        */
 
         GridPane profileInfoGridPane = new GridPane();
         profileInfoGridPane.setAlignment(Pos.CENTER_LEFT);
         profileInfoGridPane.addColumn(1, name, sex, age, dateOfBirth, nationality, languages, email, telephoneNumber, address, password);
-        profileInfoGridPane.addColumn(2, test, test, test, test, test, test, test, test, test, test);
+        profileInfoGridPane.addColumn(2, new Label(""), new Label(""), new Label(""), new Label(""), new Label(""), new Label(""),new Label(""), new Label(""), new Label(""), new Label(""), new Label(""));
 
         ColumnConstraints col1Constraints = new ColumnConstraints(400, 400, 400, Priority.NEVER, HPos.LEFT, true);
         col1Constraints.setPercentWidth(31);
@@ -205,47 +214,47 @@ public class ProfileGUI extends Application {
         VBox profileInfoPane = new VBox();
         profileInfoPane.setPadding(profileInfoInsets);
         profileInfoPane.setAlignment(Pos.TOP_LEFT);
-        profileInfoPane.getChildren().addAll(profileInfoTitlePane,profileInfoGridPane);
+        profileInfoPane.getChildren().addAll(profileInfoLabel, profileInfoGridPane);
 
         GridPane profileInfoContinuedGridPane = new GridPane();
         profileInfoContinuedGridPane.setAlignment(Pos.TOP_LEFT);
-        profileInfoContinuedGridPane.addColumn(1,university,study,studyYear);
-        ColumnConstraints continuedInfoCol1Constraints = new ColumnConstraints(300,300,300,Priority.NEVER,HPos.LEFT,true);
+        profileInfoContinuedGridPane.addColumn(1, university, study, studyYear);
+        ColumnConstraints continuedInfoCol1Constraints = new ColumnConstraints(300, 300, 300, Priority.NEVER, HPos.LEFT, true);
         continuedInfoCol1Constraints.setPercentWidth(30);
-        profileInfoContinuedGridPane.addColumn(2,test,test,test);
-        ColumnConstraints continuedInfoCol2Constraints = new ColumnConstraints(700,700,700,Priority.ALWAYS,HPos.LEFT,true);
+        profileInfoContinuedGridPane.addColumn(2, new Label(""),new Label(""),new Label(""));
+        ColumnConstraints continuedInfoCol2Constraints = new ColumnConstraints(700, 700, 700, Priority.ALWAYS, HPos.LEFT, true);
         continuedInfoCol2Constraints.setPercentWidth(70);
-        for(int i=0;i<=3;i++){
+        for (int i = 0; i <= 3; i++) {
             profileInfoContinuedGridPane.addRow(i);
-            RowConstraints rowConstraints = new RowConstraints(20,20,20,Priority.NEVER,VPos.CENTER,false);
+            RowConstraints rowConstraints = new RowConstraints(20, 20, 20, Priority.NEVER, VPos.CENTER, false);
             profileInfoContinuedGridPane.getRowConstraints().addAll(rowConstraints);
         }
 
         HBox profileInfoCoursesLabelPane = new HBox();
-        profileInfoCoursesLabelPane.setMinSize(1000,40);
-        profileInfoCoursesLabelPane.setPrefSize(1000,40);
-        profileInfoCoursesLabelPane.setMaxSize(1000,40);
+        profileInfoCoursesLabelPane.setMinSize(1000, 40);
+        profileInfoCoursesLabelPane.setPrefSize(1000, 40);
+        profileInfoCoursesLabelPane.setMaxSize(1000, 40);
         profileInfoCoursesLabelPane.setAlignment(Pos.BASELINE_LEFT);
         profileInfoCoursesLabelPane.getChildren().addAll(courses);
 
 
         GridPane profileInfoCoursesGridPane = new GridPane();
         profileInfoCoursesGridPane.setAlignment(Pos.TOP_LEFT);
-        profileInfoCoursesGridPane.addColumn(1, findATutor,becomeATutor,findStudyBuddy);
-        ColumnConstraints coursesCol1Constraints = new ColumnConstraints(300,300,300,Priority.NEVER,HPos.LEFT,true);
+        profileInfoCoursesGridPane.addColumn(1, findATutor, becomeATutor, findStudyBuddy);
+        ColumnConstraints coursesCol1Constraints = new ColumnConstraints(300, 300, 300, Priority.NEVER, HPos.LEFT, true);
         coursesCol1Constraints.setPercentWidth(30);
-        profileInfoCoursesGridPane.addColumn(2,test,test,test);
-        ColumnConstraints coursesCol2Constrains = new ColumnConstraints(700,700,700,Priority.ALWAYS,HPos.LEFT,true);
+        profileInfoCoursesGridPane.addColumn(2, new Label(""),new Label(""),new Label(""));
+        ColumnConstraints coursesCol2Constrains = new ColumnConstraints(700, 700, 700, Priority.ALWAYS, HPos.LEFT, true);
         coursesCol2Constrains.setPercentWidth(70);
-        for(int i=0;i<=3;i++){
+        for (int i = 0; i <= 3; i++) {
             profileInfoCoursesGridPane.addRow(i);
-            RowConstraints rowConstraints = new RowConstraints(20,20,20,Priority.NEVER,VPos.CENTER,false);
+            RowConstraints rowConstraints = new RowConstraints(20, 20, 20, Priority.NEVER, VPos.CENTER, false);
             profileInfoContinuedGridPane.getRowConstraints().addAll(rowConstraints);
         }
 
         VBox profileAdditionalInfoGridPaneContainerPane = new VBox();
         profileAdditionalInfoGridPaneContainerPane.setAlignment(Pos.TOP_LEFT);
-        profileAdditionalInfoGridPaneContainerPane.getChildren().addAll(profileInfoContinuedGridPane,profileInfoCoursesGridPane);
+        profileAdditionalInfoGridPaneContainerPane.getChildren().addAll(profileInfoContinuedGridPane, profileInfoCoursesGridPane);
 
         HBox profileAvailabilityTitlePane = new HBox();
         profileAvailabilityTitlePane.setPadding(profileAvailabilityTitlePaneInsets);
@@ -254,21 +263,21 @@ public class ProfileGUI extends Application {
         GridPane profileAvailabilityGridPane = new GridPane();
         profileAvailabilityGridPane.setAlignment(Pos.TOP_LEFT);
         profileAvailabilityGridPane.setPadding(profileAvailabilityGridPaneInsets);
-        profileAvailabilityGridPane.addColumn(1,monday,tuesday,wednesday,thursday,friday,saturday,sunday);
-        ColumnConstraints availabilityCol1Constraints = new ColumnConstraints(150,150,150,Priority.NEVER,HPos.LEFT,true);
+        profileAvailabilityGridPane.addColumn(1, monday, tuesday, wednesday, thursday, friday, saturday, sunday);
+        ColumnConstraints availabilityCol1Constraints = new ColumnConstraints(150, 150, 150, Priority.NEVER, HPos.LEFT, true);
         availabilityCol1Constraints.setPercentWidth(21);
-        ColumnConstraints availabilityCol2Constraints = new ColumnConstraints(550,550,550,Priority.ALWAYS,HPos.LEFT,true);
+        ColumnConstraints availabilityCol2Constraints = new ColumnConstraints(550, 550, 550, Priority.ALWAYS, HPos.LEFT, true);
         availabilityCol2Constraints.setPercentWidth(79);
 
         VBox profileAvailabilityPane = new VBox();
         profileAvailabilityPane.setPadding(profileAvailabilityPaneInsets);
         profileAvailabilityPane.setAlignment(Pos.TOP_LEFT);
-        profileAvailabilityPane.getChildren().addAll(profileAvailabilityTitlePane,profileAvailabilityGridPane);
+        profileAvailabilityPane.getChildren().addAll(profileAvailabilityTitlePane, profileAvailabilityGridPane);
 
         HBox profileAdditionalInfoPane = new HBox();
         profileAdditionalInfoPane.setPadding(profileAdditionalInfoInsets);
         profileAdditionalInfoPane.setAlignment(Pos.TOP_LEFT);
-        profileAdditionalInfoPane.getChildren().addAll(profileAdditionalInfoGridPaneContainerPane,profileAvailabilityPane);
+        profileAdditionalInfoPane.getChildren().addAll(profileAdditionalInfoGridPaneContainerPane, profileAvailabilityPane);
 
         FlowPane profile = new FlowPane(Orientation.HORIZONTAL, 20, 20);
         profile.setPadding(profileInsets);
