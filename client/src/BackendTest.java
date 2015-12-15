a1f2f89e5604f74567b89335d83fe6e873411a82
Govert de Gans <grappigegovert@hotmail.com>
2015-12-15 10:27:55 +0100

    First part of client backend

---
+++ client/src/BackendTest.java
@@ -0,0 +1,58 @@
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Govert on 15-12-2015.
+ */
+public class BackendTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void testAddMessageListener() throws Exception {
+
+    }
+
+    @Test
+    public void testOnResponse() throws Exception {
+
+    }
+
+    @Test
+    public void testConnectToServer() throws Exception {
+
+    }
+
+    @Test
+    public void testCloseConnection() throws Exception {
+
+    }
+
+    @Test
+    public void testLogin() throws Exception {
+
+    }
+
+    @Test
+    public void testLogout() throws Exception {
+
+    }
+
+    @Test
+    public void testRegister() throws Exception {
+
+    }
+
+    @Test
+    public void testIsConnected() throws Exception {
+
+    }
+
+    @Test
+    public void testGetInputStream() throws Exception {
+
+    }
+}
