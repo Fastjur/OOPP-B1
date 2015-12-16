996c554618e0b77cfe8dfd87f036e3d99217f79e
Govert de Gans <grappigegovert@hotmail.com>
2015-12-16 14:30:54 +0100

    Latest changes for clientbackend to ready merging

--- client/src/IDisconnectListener.java
+++ client/src/IDisconnectListener.java
@@ -2,5 +2,5 @@
  * Created by Govert on 15-12-2015.
  */
 public interface IDisconnectListener {
-    public void onConnectionChange(boolean errornous);
+    public void onDisconnect(boolean errornous);
 }
